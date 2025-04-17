94852577f29bef3ecd978ef81ad67ea9e617ed03
KlarkKe <tamnends@gmail.com>
2025-04-17 04:51:16 +0300

    input reader

---
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform bodyTransform;
+    [SerializeField] private Rigidbody2D rb;
+    
+    [Header("Settigns")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float turningRate = 30f;
+
+    private Vector2 previousMovementInput;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) { return; }
+        inputReader.MoveEvent += HandleMove;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner) { return; }
+        inputReader.MoveEvent -= HandleMove;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void HandleMove(Vector2 movementInput)
+    {
+        previousMovementInput = movementInput;
+    }
+}
